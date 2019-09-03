98f6efd0e40a5ca3321e8c1f7098fbc8583299cb
ZaneZam <cyxman@yahoo.com>
2019-09-03 16:03:33 +0200

    ZZ: anykernel: bring in anykernel template taken from recent franco kernel r22 zip

---
+++ anykernel/anykernel.sh
@@ -0,0 +1,53 @@
+# AnyKernel2 Ramdisk Mod Script
+# osm0sis @ xda-developers
+
+## AnyKernel setup
+# begin properties
+properties() { '
+kernel.string=Franco Kernel by franciscofranco @ xda-developers
+do.devicecheck=1
+do.modules=0
+do.cleanup=1
+do.cleanuponabort=0
+device.name1=OnePlus6
+device.name2=enchilada
+device.name3=fajita
+device.name4=OnePlus6T
+supported.versions=9
+'; } # end properties
+
+# shell variables
+block=/dev/block/bootdevice/by-name/boot;
+is_slot_device=1;
+ramdisk_compression=auto;
+
+## AnyKernel methods (DO NOT CHANGE)
+# import patching functions/variables - see for reference
+. tools/ak3-core.sh;
+
+## AnyKernel file attributes
+# set permissions/ownership for included ramdisk files
+chmod -R 750 $ramdisk/*;
+chown -R root:root $ramdisk/*;
+
+## AnyKernel install
+dump_boot;
+
+# If the kernel image and dtbs are separated in the zip
+decompressed_image=$home/kernel/Image
+compressed_image=$decompressed_image.gz
+if [ -f $compressed_image ]; then
+  # Hexpatch the kernel if Magisk is installed ('skip_initramfs' -> 'want_initramfs')
+  if [ -d $ramdisk/.backup ]; then
+    ui_print " "; ui_print "Magisk detected! Patching kernel so reflashing Magisk is not necessary...";
+    $bin/magiskboot --decompress $compressed_image $decompressed_image;
+    $bin/magiskboot --hexpatch $decompressed_image 736B69705F696E697472616D667300 77616E745F696E697472616D667300;
+    $bin/magiskboot --compress=gzip $decompressed_image $compressed_image;
+  fi;
+
+  # Concatenate all of the dtbs to the kernel
+  cat $compressed_image $home/dtbs/*.dtb > $home/Image.gz-dtb;
+fi;
+
+# Install the boot image
+write_boot;
